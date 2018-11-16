7248e89bacb34b62d495887d0a89aa7e4fe90819
josimar-junior <juniorpaz99@gmail.com>
2018-11-16 00:10:37 -0300

    Component of restaurant ratings

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html'
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>;
+
+  constructor(private restaurantsService: RestaurantsService,
+              private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.getReviewsOfRestaurant(this.router.parent.snapshot.params['id']);
   }
 
 }
