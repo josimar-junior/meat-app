ff1a7cae2815a3b27180020057e50f781f21f83d
josimar-junior <juniorpaz99@gmail.com>
2018-11-21 23:29:30 -0300

    Implementing the shopping cart

--- src/app/restaurant-detail/shopping-carts/shopping-carts.component.ts
+++ src/app/restaurant-detail/shopping-carts/shopping-carts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-carts.service';
 
 @Component({
   selector: 'mt-shopping-carts',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
 
+  items(): any[] {
+    return this.shoppingCartService.items;
+  }
+
+  total(): number {
+    return this.shoppingCartService.total();
+  }
 }
