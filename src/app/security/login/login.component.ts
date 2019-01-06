cbf36f157ad5d693e5b71f981c43234d37f9b485
josimar-junior <juniorpaz99@gmail.com>
2019-01-05 21:06:11 -0300

    Using CanActive to protect the component

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
       email: this.formBuilder.control('', [Validators.required, Validators.email]),
       password: this.formBuilder.control('', [Validators.required])
     });
-    this.navigateTo = this.activatedRoute.snapshot.params['to'] || '/';
+    this.navigateTo = this.activatedRoute.snapshot.params['to'] || btoa('/');
   }
 
   login() {
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
       this.notificationService.notify(`Bem vindo(a), ${user.name}`),
       response => this.notificationService.notify(response.error.message),
       () => {
-        this.router.navigate([this.navigateTo]);
+        this.router.navigate([atob(this.navigateTo)]);
       });
   }
 
